ed3304bde4b664ffd1f4dfcbdca0dcdad89f65a8
Andrei Babikov <andrei.babikov@gmail.com>
2022-03-31 20:31:31 +0300

    feature/update on post and metrics model

--- internal/handlers/update.go
+++ internal/handlers/update.go
@@ -1,9 +1,11 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 	"strconv"
 
+	"github.com/andrei-cloud/go-devops/internal/model"
 	"github.com/andrei-cloud/go-devops/internal/repo"
 	"github.com/go-chi/chi"
 )
@@ -41,5 +43,42 @@ func Update(repo repo.Repository) http.HandlerFunc {
 		}
 
 		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func UpdatePost(repo repo.Repository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("Content-Type") != "application/json" {
+			http.Error(w, "invalid content type", http.StatusInternalServerError)
+		}
+
+		metrics := model.Metrics{}
+		if err := json.NewDecoder(r.Body).Decode(&metrics); err != nil {
+			http.Error(w, "invalid resquest", http.StatusInternalServerError)
+		}
+
+		switch metrics.MType {
+		case "gauge":
+			if metrics.Value != nil {
+				if err := repo.UpdateGauge(r.Context(), metrics.ID, *metrics.Value); err != nil {
+					http.Error(w, "failed to update", http.StatusInternalServerError)
+					return
+				}
+			} else {
+				http.Error(w, "invalid resquest", http.StatusBadRequest)
+			}
+		case "counter":
+			if metrics.Delta != nil {
+				if err := repo.UpdateCounter(r.Context(), metrics.ID, *metrics.Delta); err != nil {
+					http.Error(w, "failed to update", http.StatusInternalServerError)
+					return
+				}
+			} else {
+				http.Error(w, "invalid resquest", http.StatusBadRequest)
+			}
+		default:
+			http.Error(w, "invalid metric type", http.StatusNotImplemented)
+			return
+		}
 	}
 }
--- internal/router/router.go
+++ internal/router/router.go
@@ -15,9 +15,7 @@ func SetupRouter() *chi.Mux {
 	//r.Use(middleware.Recoverer)
 	r.Get("/value/{m_type}/{m_name}", handlers.GetMetrics(repo))
 	r.Post("/update/{m_type}/{m_name}/{value}", handlers.Update(repo))
-	// r.Post("/update/", func(w http.ResponseWriter, r *http.Request) {
-	// 	http.Error(w, "invalid metric type", http.StatusNotImplemented)
-	// })
+	r.Post("/update", handlers.UpdatePost(repo))
 
 	return r
 }
