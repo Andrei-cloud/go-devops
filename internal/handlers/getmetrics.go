4d618bdcf307d753e1906a2c49e50c0fc52ef1fa
Andrei Babikov <andrei.babikov@gmail.com>
2022-04-01 07:40:52 +0300

    feature/getmetrics post

--- internal/handlers/getmetrics.go
+++ internal/handlers/getmetrics.go
@@ -1,9 +1,11 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
+	"github.com/andrei-cloud/go-devops/internal/model"
 	"github.com/andrei-cloud/go-devops/internal/repo"
 	"github.com/go-chi/chi"
 )
@@ -31,6 +33,45 @@ func GetMetrics(repo repo.Repository) http.HandlerFunc {
 		default:
 			http.Error(w, "invalid metric type", http.StatusNotImplemented)
 			return
+		}
+	}
+}
+
+func GerMetricsPost(repo repo.Repository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("Content-Type") != "application/json" {
+			http.Error(w, "invalid content type", http.StatusInternalServerError)
+		}
+
+		metrics := model.Metrics{}
+		if err := json.NewDecoder(r.Body).Decode(&metrics); err != nil {
+			http.Error(w, "invalid resquest", http.StatusInternalServerError)
+		}
+
+		switch metrics.MType {
+		case "gauge":
+			result, err := repo.GetGauge(r.Context(), metrics.ID)
+			if err != nil {
+				http.Error(w, "not found", http.StatusNotFound)
+				return
+			}
+			metrics.Value = &result
+		case "counter":
+			result, err := repo.GetCounter(r.Context(), metrics.ID)
+			if err != nil {
+				http.Error(w, "not found", http.StatusNotFound)
+				return
+			}
+			metrics.Delta = &result
+		default:
+			http.Error(w, "invalid metric type", http.StatusNotImplemented)
+			return
+		}
+
+		if resp, err := json.Marshal(metrics); err != nil {
+			http.Error(w, "failed to build response", http.StatusInternalServerError)
+		} else {
+			w.Write(resp)
 		}
 	}
 }
--- internal/router/router.go
+++ internal/router/router.go
@@ -15,7 +15,8 @@ func SetupRouter() *chi.Mux {
 	//r.Use(middleware.Recoverer)
 	r.Get("/value/{m_type}/{m_name}", handlers.GetMetrics(repo))
 	r.Post("/update/{m_type}/{m_name}/{value}", handlers.Update(repo))
-	r.Post("/update", handlers.UpdatePost(repo))
+	r.Post("/update/", handlers.UpdatePost(repo))
+	r.Post("/value/", handlers.GerMetricsPost(repo))
 
 	return r
 }
