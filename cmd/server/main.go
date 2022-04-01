f7025552aea359ca9a881d8ebc152c21fc6c3c1e
Andrei Babikov <andrei.babikov@gmail.com>
2022-04-01 13:30:14 +0300

    feature/server env setting and refactor

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,39 +1,11 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
-
-	"github.com/andrei-cloud/go-devops/internal/router"
-)
+import "github.com/andrei-cloud/go-devops/internal/server"
 
 func main() {
+	s := server.NewServer()
 
-	r := router.SetupRouter()
+	s.Run()
 
-	s := &http.Server{
-		Addr:           ":8080",
-		Handler:        r,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
-
-	go s.ListenAndServe()
-
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
-	<-sig
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	if err := s.Shutdown(ctx); err != nil {
-		fmt.Println(err)
-	}
-	//fmt.Println("server quit")
+	s.Shutdown() //blocking function
 }
---
+++ internal/server/server.go
@@ -0,0 +1,64 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/andrei-cloud/go-devops/internal/router"
+
+	"github.com/caarlos0/env"
+	"github.com/go-chi/chi"
+)
+
+var cfg Config
+
+type Config struct {
+	Address string `env:"ADDRESS" envDefault:"127.0.0.1:8080"`
+}
+
+type server struct {
+	r *chi.Mux
+	s *http.Server
+}
+
+func init() {
+	cfg = Config{}
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func NewServer() *server {
+	srv := server{}
+	srv.r = router.SetupRouter()
+	srv.s = &http.Server{
+		Addr:           cfg.Address,
+		Handler:        srv.r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	return &srv
+}
+
+func (srv *server) Run() {
+	go srv.s.ListenAndServe()
+}
+
+func (srv *server) Shutdown() {
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+	<-sig
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.s.Shutdown(ctx); err != nil {
+		fmt.Println(err)
+	}
+}
