8705c18c9eb778e980e1093fe13c17dd7b52509a
Andrei Babikov <andrei.babikov@gmail.com>
2022-07-20 21:47:01 +0300

    increment-19

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,13 +2,30 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/rs/zerolog/log"
 
 	"github.com/andrei-cloud/go-devops/internal/server"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
 func main() {
+	if buildVersion == "" {
+		buildVersion = "N/A"
+	}
+	if buildDate == "" {
+		buildDate = "N/A"
+	}
+	if buildCommit == "" {
+		buildCommit = "N/A"
+	}
+	fmt.Printf("Build version: %s\nBuild date: %s\nBuild commit: %s\n", buildVersion, buildDate, buildCommit)
 	s := server.NewServer()
 
 	ctx, cancel := context.WithCancel(context.Background())
