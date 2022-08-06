bb69337af57e134a207d1d23e30d48b84b4e41e3
Andrei Babikov <andrei.babikov@gmail.com>
2022-08-06 14:19:54 +0300

    reworked test on suite

--- internal/storage/persistent/db_test.go
+++ internal/storage/persistent/db_test.go
@@ -4,147 +4,116 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	"log"
 	"testing"
 
+	"github.com/andrei-cloud/go-devops/internal/repo"
 	_ "github.com/jackc/pgx/v4/stdlib"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 
 	"github.com/DATA-DOG/go-sqlmock"
 )
 
-func newMock() (*sql.DB, sqlmock.Sqlmock) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		log.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-
-	return db, mock
+type DBTestSuite struct {
+	suite.Suite
+	db   *sql.DB
+	mock sqlmock.Sqlmock
+	repo repo.Repository
 }
 
-func Test_storage_Ping(t *testing.T) {
-	db, _ := newMock()
-	repo := &storage{db}
-	defer func() {
-		repo.Close()
-	}()
-
-	assert.NoError(t, repo.Ping())
+func (s *DBTestSuite) SetupTest() {
+	var err error
+	s.db, s.mock, err = sqlmock.New()
+	s.repo = &storage{s.db}
+	s.NoError(err)
 }
 
-func Test_storage_UpdateGauge(t *testing.T) {
-	db, mock := newMock()
-	repo := &storage{db}
-	defer func() {
-		repo.Close()
-	}()
+func (s *DBTestSuite) TearDownTest() {
+	s.repo.Close()
+}
 
+func (s *DBTestSuite) TestPing() {
+	s.NoError(s.repo.Ping())
+}
+
+func (s *DBTestSuite) TestUpdateGauge() {
 	query := "^insert into metrics (.+)"
 
-	mock.ExpectExec(query).WithArgs("test", 1.234).WillReturnResult(sqlmock.NewResult(1, 1))
-	mock.ExpectExec(query).WithArgs("fail", 1.234).WillReturnError(fmt.Errorf("DB error"))
-	assert.NoError(t, repo.UpdateGauge(context.Background(), "test", 1.234))
-	assert.Error(t, repo.UpdateGauge(context.Background(), "fail", 1.234))
-
+	s.mock.ExpectExec(query).WithArgs("test", 1.234).WillReturnResult(sqlmock.NewResult(1, 1))
+	s.mock.ExpectExec(query).WithArgs("fail", 1.234).WillReturnError(fmt.Errorf("DB error"))
+	s.NoError(s.repo.UpdateGauge(context.Background(), "test", 1.234))
+	s.Error(s.repo.UpdateGauge(context.Background(), "fail", 1.234))
 }
 
-func Test_storage_UpdateCounter(t *testing.T) {
-	db, mock := newMock()
-	repo := &storage{db}
-	defer func() {
-		repo.Close()
-	}()
-
+func (s *DBTestSuite) TestUpdateCounter() {
 	query := "^insert into metrics (.+)"
 
-	mock.ExpectExec(query).WithArgs("test", 1234).WillReturnResult(sqlmock.NewResult(1, 1))
-	mock.ExpectExec(query).WithArgs("fail", 1234).WillReturnError(fmt.Errorf("DB error"))
-	assert.NoError(t, repo.UpdateCounter(context.Background(), "test", 1234))
-	assert.Error(t, repo.UpdateCounter(context.Background(), "fail", 1234))
+	s.mock.ExpectExec(query).WithArgs("test", 1234).WillReturnResult(sqlmock.NewResult(1, 1))
+	s.mock.ExpectExec(query).WithArgs("fail", 1234).WillReturnError(fmt.Errorf("DB error"))
+	s.NoError(s.repo.UpdateCounter(context.Background(), "test", 1234))
+	s.Error(s.repo.UpdateCounter(context.Background(), "fail", 1234))
 }
 
-func Test_storage_GetCounter(t *testing.T) {
-	db, mock := newMock()
-	repo := &storage{db}
-	defer func() {
-		repo.Close()
-	}()
-
+func (s *DBTestSuite) TestGetCounter() {
 	query := "^SELECT delta FROM metrics WHERE mtype = 'counter' (.+)"
 
 	rows := sqlmock.NewRows([]string{"delta"}).AddRow(123)
-	mock.ExpectQuery(query).WithArgs("test").WillReturnRows(rows)
-	mock.ExpectQuery(query).WithArgs("fail").WillReturnError(fmt.Errorf("DB error"))
-	value, err := repo.GetCounter(context.Background(), "test")
-	assert.Equal(t, value, int64(123))
-	assert.NoError(t, err)
+	s.mock.ExpectQuery(query).WithArgs("test").WillReturnRows(rows)
+	s.mock.ExpectQuery(query).WithArgs("fail").WillReturnError(fmt.Errorf("DB error"))
+	value, err := s.repo.GetCounter(context.Background(), "test")
+	s.Equal(value, int64(123))
+	s.NoError(err)
 
-	_, err = repo.GetCounter(context.Background(), "fail")
-	assert.Error(t, err)
-
+	_, err = s.repo.GetCounter(context.Background(), "fail")
+	s.Error(err)
 }
 
-func Test_storage_GetGauge(t *testing.T) {
-	db, mock := newMock()
-	repo := &storage{db}
-	defer func() {
-		repo.Close()
-	}()
-
+func (s *DBTestSuite) TestGetGauge() {
 	query := "^SELECT value FROM metrics WHERE mtype = 'gauge' (.+)"
 
 	rows := sqlmock.NewRows([]string{"value"}).AddRow(1.234)
-	mock.ExpectQuery(query).WithArgs("test").WillReturnRows(rows)
-	mock.ExpectQuery(query).WithArgs("fail").WillReturnError(fmt.Errorf("DB error"))
-	value, err := repo.GetGauge(context.Background(), "test")
-	assert.Equal(t, value, float64(1.234))
-	assert.NoError(t, err)
+	s.mock.ExpectQuery(query).WithArgs("test").WillReturnRows(rows)
+	s.mock.ExpectQuery(query).WithArgs("fail").WillReturnError(fmt.Errorf("DB error"))
+	value, err := s.repo.GetGauge(context.Background(), "test")
+	s.Equal(value, float64(1.234))
+	s.NoError(err)
 
-	_, err = repo.GetGauge(context.Background(), "fail")
-	assert.Error(t, err)
+	_, err = s.repo.GetGauge(context.Background(), "fail")
+	s.Error(err)
 
 }
 
-func Test_storage_GetGaugeAll(t *testing.T) {
-	db, mock := newMock()
-	repo := &storage{db}
-	defer func() {
-		repo.Close()
-	}()
-
+func (s *DBTestSuite) TestGetGaugeAll() {
 	query := "^SELECT id, value FROM metrics WHERE mtype = 'gauge'"
 
 	rows := sqlmock.NewRows([]string{"id", "value"}).
 		AddRow("one", 1.234).
 		AddRow("two", 4.321)
-	mock.ExpectQuery(query).WillReturnRows(rows)
-	mock.ExpectQuery(query).WillReturnError(fmt.Errorf("DB error"))
-	values, err := repo.GetGaugeAll(context.Background())
-	assert.Equal(t, values["one"], float64(1.234))
-	assert.NoError(t, err)
+	s.mock.ExpectQuery(query).WillReturnRows(rows)
+	s.mock.ExpectQuery(query).WillReturnError(fmt.Errorf("DB error"))
+	values, err := s.repo.GetGaugeAll(context.Background())
+	s.Equal(values["one"], float64(1.234))
+	s.NoError(err)
 
-	_, err = repo.GetGaugeAll(context.Background())
-	assert.Error(t, err)
+	_, err = s.repo.GetGaugeAll(context.Background())
+	s.Error(err)
 }
 
-func Test_storage_GetCounterAll(t *testing.T) {
-	db, mock := newMock()
-	repo := &storage{db}
-	defer func() {
-		repo.Close()
-	}()
-
+func (s *DBTestSuite) TestGetCounterAll() {
 	query := "^SELECT id, delta FROM metrics WHERE mtype = 'counter'"
 
 	rows := sqlmock.NewRows([]string{"id", "value"}).
 		AddRow("one", 1234).
 		AddRow("two", 4321)
-	mock.ExpectQuery(query).WillReturnRows(rows)
-	mock.ExpectQuery(query).WillReturnError(fmt.Errorf("DB error"))
-	values, err := repo.GetCounterAll(context.Background())
-	assert.Equal(t, values["one"], int64(1234))
-	assert.NoError(t, err)
+	s.mock.ExpectQuery(query).WillReturnRows(rows)
+	s.mock.ExpectQuery(query).WillReturnError(fmt.Errorf("DB error"))
+	values, err := s.repo.GetCounterAll(context.Background())
+	s.Equal(values["one"], int64(1234))
+	s.NoError(err)
 
-	_, err = repo.GetCounterAll(context.Background())
-	assert.Error(t, err)
+	_, err = s.repo.GetCounterAll(context.Background())
+	s.Error(err)
+}
+
+func TestDBTestSuite(t *testing.T) {
+	suite.Run(t, new(DBTestSuite))
 }
